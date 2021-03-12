424a65b1b5628618c8fa20fe4e3dbcb1db124cfa
Eduardo Baeza <eduardo.baeza@yahoo.com>
2021-03-11 23:10:05 -0600

    Fix EsLint error on index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,18 @@
 import './style.css';
 
-import {
-  Loader
-} from "@googlemaps/js-api-loader"
+import getWeather from './asset/getWeather';
+import validate from './asset/validate';
+import currentCountry from './asset/currentCountry';
 
-import getWeather from './asset/getWeather'
-import validate from './asset/validate'
-import currentCountry from './asset/currentCountry'
-//import initMap from './asset/initMap'
-
-let unit = 'standard';
+const unit = 'standard';
 let city = '';
-let degree = '';
-let coords = '';
 
 currentCountry();
-
 
 const searchBtn = document.getElementById('search');
 searchBtn.addEventListener('click', () => {
   city = document.getElementById('city');
   if (validate(city.value) === false) {
-    console.log('entre despues de validation')
-  getWeather(city.value, unit, 'search')
-}
-})
-
-
+    getWeather(city.value, unit, 'search');
+  }
+});
